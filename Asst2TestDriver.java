ad697025c61e3c72b5b092120a379520b4efebd9
aalata <aalata@ucalgary.ca>
2015-10-20 12:28:10 -0600

    Fixed more weird tabs

--- Asst2TestDriver.java
+++ Asst2TestDriver.java
@@ -28,44 +28,44 @@ public class Asst2TestDriver
     {
 		Class objInspectClass=null;
 		try
-			{
-				objInspectClass = Class.forName(ObjectInspectorName);
-				ObjInspector = objInspectClass.newInstance();
-			}
+		{
+			objInspectClass = Class.forName(ObjectInspectorName);
+			ObjInspector = objInspectClass.newInstance();
+		}
 		catch(Exception e) 
-			{
-				throw new Exception("Unable create instance of your object inspector");
-			}
+		{
+			throw new Exception("Unable create instance of your object inspector");
+		}
 
 		// get reference to inspect method
 		try
-			{
-				Class[] param = { Object.class, boolean.class };
-				inspectionMethod = objInspectClass.getDeclaredMethod("inspect",param);
-			}
+		{
+			Class[] param = { Object.class, boolean.class };
+			inspectionMethod = objInspectClass.getDeclaredMethod("inspect",param);
+		}
 		catch(Exception e) 
-			{
-				throw new Exception("Unable to find required method: public void inspect(Object obj,boolean recursive)");
-			}
+		{
+			throw new Exception("Unable to find required method: public void inspect(Object obj,boolean recursive)");
+		}
     }
     //--------------------------------------------------------------------
     public void runTest(Object testObj) throws Exception
     {
 		try
-			{
-				System.out.println("======================================================");
-				System.out.println("Running Test: " + testObj);
-				Object[] param = { testObj, new Boolean(recursive) };
-				inspectionMethod.invoke(ObjInspector, param); 
-				System.out.println("======================================================");
-			}
+		{
+			System.out.println("======================================================");
+			System.out.println("Running Test: " + testObj);
+			Object[] param = { testObj, new Boolean(recursive) };
+			inspectionMethod.invoke(ObjInspector, param); 
+			System.out.println("======================================================");
+		}
 		catch(Exception e)
-			{
-			
-				e.printStackTrace();
-				throw new Exception("unable to compleatly run test");
-			
-			}
+		{
+		
+			e.printStackTrace();
+			throw new Exception("unable to compleatly run test");
+		
+		}
     }
     //------- Fields -----------------------------------------------------
     private Object ObjInspector = null;
@@ -77,11 +77,11 @@ public class Asst2TestDriver
 	boolean rec=true;
 
 	if(args.length >= 1)
-	    {
+	{
 		if(args.length > 1) rec = Boolean.parseBoolean(args[1]); // Bug fixed
 
 		try
-		    {
+		{
 			System.out.println("Loading object inspector: " + args[0]);
 			Asst2TestDriver driver = new Asst2TestDriver(args[0],rec);
 			driver.runTest( new ClassA() );
@@ -92,14 +92,14 @@ public class Asst2TestDriver
 			driver.runTest( new ClassB[12] );
 			driver.runTest( new ClassB[12][12] );	
 			driver.runTest( "Test String" );
-		    }
+		}
 		catch(Exception e)
-		    {
-			
+		{
+		
 			System.out.println("ERROR: " + e.getMessage());
 			System.out.println("Exiting test driver");
-		    }
-	    }
+		}
+	}
 	else
 	    System.out.println("usage: java Asst2TestDriver <object inspector name> [false]\n\nFirst argument is the name of the object inspector class to load, this class should be in the current directory. the second argument is optional, if set to false the recursive introspection will not be run for the tests.\n\n");
 
